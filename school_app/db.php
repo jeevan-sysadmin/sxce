ce882ed3c703b68e2e7114798e701c62ebf82f4b
Jeevan larosh <jeevan2k3@gmail.com>
2025-05-23 08:21:23 +0530

    Update db.php

--- school_app/db.php
+++ school_app/db.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pass = ""; // Use your MySQL password if set
+$pass = ">{vai/kgvr.q"; // Use your MySQL password if set
 $db = "school_app";
 
 $conn = new mysqli($host, $user, $pass, $db);
